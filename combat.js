4b3ea3998fecdc84668f67402446e952b59ec56c
Nathan Fenner <nfenneremail@gmail.com>
2015-04-01 01:31:12 -0400

    enemy setup

--- combat.js
+++ combat.js
@@ -1,31 +1,43 @@
 
-var enemyHealth = 0;
-var enemyMaxHealth = 0;
+var enemyName = "";
 
-var enemyAttack = 0;
-var enemyArmor = 0;
+var enemyStats = {};
+enemyStats.goblin = 			{ health: 30	, attack: 3		, defense: 10	, experience: 15	 };
+enemyStats["goblin chief"] = 	{ health: 60	, attack: 6		, defense: 20	, experience: 30	 };
+enemyStats.troll = 				{ health: 150	, attack: 40	, defense: 10	, experience: 40	 };
+enemyStats.dragon = 			{ health: 30000	, attack: 300	, defense: 98	, experience: 10000	 };
+enemyStats["ancient dragon"] =	{ health: 30000	, attack: 200	, defense: 99	, experience: 20000	 };
+
+function setupEnemy(name) {
+	enemyName = name;
+	enemyHealth = stats[enemyName].health;
+	enemyStats[enemyName].attack
+}
+
+
+var enemyHealth = 0;
 
 function enemyLiving() {
 	return enemyHealth > 0;
 }
 
 function enemyTakeDamage(amount) {
-	amount = Math.ceil( amount * (1 - enemyArmor / 100) );
+	amount = Math.ceil( amount * (1 - enemyStats[enemyName].defense / 100) );
 	enemyHealth -= amount;
 	enemyHealth = Math.max(0, enemyHealth);
 }
 
 function enemyHeal(amount) {
 	enemyHealth += amount;
-	enemyHealth = Math.min(enemyHealth, enemyMaxHealth);
+	enemyHealth = Math.min(enemyHealth, enemyStats[enemyName].health);
 }
 
 function enemyMaxHeal() {
-	enemyHealth = enemyMaxHealth;
+	enemyHealth = enemyStats[enemyName].health;
 }
 
 function getEnemyAttack() {
-	var amount = Math.ceil( enemyAttack * (1 + Math.random()) / 2);
+	var amount = Math.ceil( enemyStats[enemyName].attack * (1 + Math.random()) / 2);
 	return amount;
 }
 

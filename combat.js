34e81f497ee35cb0fe65c71a97cdf9a8d9b4d35d
Nathan Fenner <nfenneremail@gmail.com>
2015-04-01 01:58:10 -0400

    much improved combat interface

--- combat.js
+++ combat.js
@@ -8,10 +8,14 @@ enemyStats.troll = 				{ health: 150	, attack: 40	, defense: 10	, experience: 40
 enemyStats.dragon = 			{ health: 30000	, attack: 300	, defense: 98	, experience: 10000	 };
 enemyStats["ancient dragon"] =	{ health: 30000	, attack: 200	, defense: 99	, experience: 20000	 };
 
-function setupEnemy(name) {
+function enterCombat(name) {
 	enemyName = name;
 	enemyHealth = stats[enemyName].health;
-	enemyStats[enemyName].attack
+	enemyStats[enemyName].attack;
+	document.getElementById("site").style.display = "none";
+	document.getElementById("map").style.display = "none";
+	document.getElementById("combat").style.display = "combat";
+	combatRender();
 }
 
 
@@ -40,4 +44,62 @@ function getEnemyAttack() {
 	var amount = Math.ceil( enemyStats[enemyName].attack * (1 + Math.random()) / 2);
 	return amount;
 }
+
+function combatRender() {
+	document.getElementById("combat_enemy_name").innerHTML = nameCase(enemyName);
+	document.getElementById("combat_enemy_health").innerHTML = "Enemy Health: " + enemyHealth + "/" + enemyMaxHeal;
+	document.getElementById("combat_enemy_attack").innerHTML = "Enemy Attack: " + enemyStats[enemyName].attack;
+	document.getElementById("combat_enemy_defense").innerHTML = "Enemy Defense: " + enemyStats[enemyName].defense;
+}
+
+function enemyTurn() {
+	playerTakeDamage( getEnemyAttack() );
+}
+
+function combatAdvance() {
+	if (!playerLiving()) {
+		alert("You died!");
+	}
+	if (!enemyLiving()) {
+		combatRender();
+		return leaveCombat();
+	}
+	enemyTurn();
+	if (!playerLiving()) {
+		alert("You died!");
+	}
+	if (!enemyLiving()) {
+		combatRender();
+		return leaveCombat();
+	}
+	combatRender();
+}
+
+
+// Returns the UI that was hidden when the encounter started
+function leaveCombat() {
+	document.getElementById("site").style.display = "block";
+	document.getElementById("map").style.display = "block";
+	document.getElementById("combat").style.display = "none";
+	playerMode = "explore";
+}
+
+document.getElementById("combat_button_attack").onmousedown = function() {
+	enemyTakeDamage( getPlayerAttack() );
+	combatAdvance();
+};
+
+document.getElementById("combat_button_flee").onmousedown = function() {
+	if (Math.random() < 0.25) {
+		alert("You got away!");
+		return leaveCombat();
+	} else {
+		alert("Your enemy prevents your retreat!");
+		combatAdvance();
+	}
+};
+
+document.getElementById("combat_button_wait").onmousedown = function() {
+	combatAdvance();
+};
 

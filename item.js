7b186b84c615bdf023b62a0778cfb484769a3cba
Nathan Fenner <nfenneremail@gmail.com>
2015-04-02 00:31:17 -0400

    major changes to items

--- item.js
+++ item.js
@@ -18,43 +18,127 @@ renderInventory()
 
 */
 
-var playerItems = [];
-var playerGold = 0;
-var playerWeapon = "fists";
-var playerArmor = "cloth";
-
-var descriptions = {};
-descriptions.sword = "A steel sword"
-
-var stats = {};
-stats.fists = {type:"weapon",attack:2};
-stats.dagger = {type:"weapon",attack:10};
-stats.sword = {type:"weapon",attack:15};
-stats.warhammer = {type:"weapon",attack:25};
-stats["sword of flames"] = {type:"weapon",attack:35};
-
-stats["cloth"] = {type:"armor", defense:2};
-stats["leather armor"] = {type:"armor", defense:20};
-stats["iron platemail"] = {type:"armor", defense:40};
-stats["mithril chainmail"] = {type:"armor", defense:85};
-stats["ringmail of light"] = {type:"armor", defense:90};
+/*
+REQUIRED:
+.name
+.type
+.describe() -- give short description
 
 
-function acquireItem(name) {
-	if (typeof(name) !== "string" || !stats[name]) {
+.useSelf() -- use on self
+.useSelfPeaceful() -- use on self only in peace
+.useSelfCombat() -- use on self only in combat
+.useEnemy() -- use on enemy (requires combat)
+.longDescribe() -- optional long description
+
+For particular types:
+
+"weapon"
+required:
+	.attack
+optional:
+	.beforeAttack(target)
+	.afterAttack(target)
+	.parry(target)
+
+"armor"
+required:
+	.defense
+optional:
+	.beforeBlock(target)
+	.afterBlock(target)
+
+"consumable"
+required:
+	.consume(self)
+
+
+
+*/
+
+
+function weaponDescribe(weapon) {
+	return nameCase(weapon.name) + " (+" + weapon.attack + ")";
+}
+
+function armorDescribe(armor) {
+	return nameCase(armor.name) + " [+" + armor.defense + "]";
+}
+
+var itemDataTable = {};
+itemDataTable.weapon = {type:"weapon", describeFun: weaponDescribe};
+itemDataTable.armor = {type:"armor", describeFun: armorDescribe};
+
+itemDataTable["fists"] = {parent:"weapon", attack:2};
+itemDataTable["copper dagger"] = {parent:"weapon", attack: 10};
+itemDataTable["steel sword"] = {parent:"weapon", attack: 10};
+itemDataTable["war axe"] = {parent:"weapon", attack: 30};
+itemDataTable["spear"] = {parent:"weapon", attack: 20};
+itemDataTable["spectral sword of despair"] = {parent:"weapon", attack: 80};
+
+itemDataTable["cloth"] = {parent:"armor", defense: 5};
+itemDataTable["leather armor"] = {parent:"armor", defense: 10};
+itemDataTable["iron platemail"] = {parent:"armor", defense: 20};
+itemDataTable["chainmail"] = {parent:"armor", defense: 50};
+itemDataTable["mithril platemail"] = {parent:"armor", defense: 75};
+itemDataTable["mithril chainmail"] = {parent:"armor", defense: 80};
+
+
+
+function Item(name) {
+	if (!itemDataTable[name]) {
 		throw "no such item " + name;
 	}
-	playerItems.push(name);
+	this.name = name;
+	var data = itemDataTable[name];
+	do {
+		for (var i in data) {
+			if (i !== "parent") {
+				this[i] = data[i];
+			}
+		}
+		data = data.parent && itemDataTable[data.parent];
+	} while (data);
+}
+Item.prototype.describe = function() {
+	return this.describeFun(this);
+};
+
+
+
+var playerItems = [];
+var playerGold = 0;
+var playerWeapon = new Item("fists");
+var playerArmor = new Item("cloth");
+
+function acquireItem(item) {
+	if (!(item instanceof Item)) {
+		throw "not an item";
+	}
+	playerItems.push(item);
 	renderInventory();
 }
 
 function dropItem(name, amount) {
+
+	if (name instanceof Item) {
+		for (var i = 0; i < playerItems.length; ++i) {
+			if (playerItems[i] === name) {
+				playerItems.splice(i, 1);
+				renderInventory();
+				return;
+			}
+		}
+		throw "no such item in inventory";
+	}
+
+
 	if (typeof name !== "string" || !stats[name]) {
 		throw "no such item " + name;
 	}
 	if (typeof amount === "undefined") {
 		for (var i = 0; i < playerItems.length; ++i) {
-			if (playerItems[i] === name) {
+			if (playerItems[i].name === name) {
 				playerItems.splice(i, 1);
 				renderInventory();
 				return;
@@ -80,7 +164,7 @@ function countItem(name) {
 	}
 	var count = 0;
 	for (var i = 0; i < playerItems.length; ++i) {
-		if (playerItems[i] === name) {
+		if (playerItems[i].name === name) {
 			++count;
 		}
 	}
@@ -117,43 +201,37 @@ function hasGold(amount) {
 	return playerGold >= amount;
 }
 
-function equipWeapon(name) {
-	if (!stats[name] || stats[name].type !== "weapon") {
-		throw "not a weapon " + name;
-	}
-	dropItem(name);
-	if (playerWeapon !== "fists") {
+function equipWeapon(item) {
+	dropItem(item);
+	if (playerWeapon.name !== "fists") {
 		acquireItem(playerWeapon);
 	}
-	playerWeapon = name;
+	playerWeapon = item;
 	renderInventory();
 }
 
-function equipArmor(name) {
-	if (!stats[name] || stats[name].type !== "armor") {
-		throw "not an armor " + name;
-	}
-	dropItem(name);
-	if (playerArmor !== "cloth") {
+function equipArmor(item) {
+	dropItem(item);
+	if (playerArmor.name !== "cloth") {
 		acquireItem(playerArmor);
 	}
-	playerArmor = name;
+	playerArmor = item;
 	renderInventory();
 }
 
 function unequipWeapon() {
-	if (playerWeapon !== "fists") {
+	if (playerWeapon.name !== "fists") {
 		acquireItem(playerWeapon);
 	}
-	playerWeapon = "fists";
+	playerWeapon = new Item("fists");
 	renderInventory();
 }
 
 function unequipArmor() {
-	if (playerArmor !== "cloth") {
+	if (playerArmor.name !== "cloth") {
 		acquireItem(playerArmor);
 	}
-	playerArmor = "cloth";
+	playerArmor = new Item("cloth");
 	renderInventory();
 }
 
@@ -161,14 +239,15 @@ function unequipArmor() {
 
 function useItem(item) {
 
-	if (!stats[item]) {
-		throw "not a valid item " + item;
+	if (!(item instanceof Item)) {
+		throw "not a valid item";
 	}
+
 	var doesSomething = true;
-	var stat = stats[item];
-	if (stat.type === "weapon") {
+
+	if (item.type === "weapon") {
 		equipWeapon(item);
-	} else if (stat.type === "armor") {
+	} else if (item.type === "armor") {
 		equipArmor(item);
 	} else {
 		doesSomething = false;
@@ -192,33 +271,22 @@ function nameCase(s) {
 	return s.charAt(0).toUpperCase() + s.substring(1);
 }
 
-function describe(name) {
-	var base = nameCase(name);
-	var item = stats[name];
-	if (item.type === "armor") {
-		return base + " " + item.defense + "%";
-	} else if (item.type === "weapon") {
-		return base + " +" + item.attack;
-	} else {
-		return base;
-	}
-}
 
 function renderInventory() {
 	var list = document.createElement("ul");
 
 	var weapon = document.createElement("li");
-	weapon.innerHTML = "[EQUIP] " + describe( playerWeapon );
+	weapon.innerHTML = "[Weapon] " + playerWeapon.describe();
 	list.appendChild(weapon);
 
 	var armor = document.createElement("li");
-	armor.innerHTML = "[EQUIP] " + describe( playerArmor );
+	armor.innerHTML = "[Armor] " + playerArmor.describe();
 	list.appendChild(armor);
 
 	for (var i = 0; i < playerItems.length; ++i) {
 		var item = playerItems[i];
 		var li = document.createElement("li");
-		li.innerHTML = describe(item) + " ";
+		li.innerHTML = item.describe() + " ";
 		var button = document.createElement("button");
 		button.innerHTML = "Use";
 		button.onmousedown = useItemClosure(item);

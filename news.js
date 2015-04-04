451d4b3f3a937c508c7a40eef7c19cdc4c96c8e3
Nathan Fenner <nfenneremail@gmail.com>
2015-04-03 21:27:09 -0400

    addNews created

---
+++ news.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+var newsList = [];
+
+function addNews(news) {
+	newsList.push(news);
+	renderNews();
+}
+
+function renderNews() {
+	var news = document.getElementById("news");
+	news.innerHTML = "";
+	var ul = document.createElement("ul");
+	for (var i = 0; i < newsList.length; ++i) {
+		var li = document.createElement("li");
+		li.innerHTML = newsList[i];
+		ul.appendChild(li);
+	}
+	news.appendChild(ul);
+}

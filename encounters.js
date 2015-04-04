8d3a594ece8ebd686ebc1f16ede37d7f0566f308
CurtisFenner <cfenneremail@gmail.com>
2015-04-03 20:59:06 -0400

    unneeded deleted

--- encounters.js
+++
@@ -1,5 +0,0 @@
-// Returns the UI that was hidden when the encounter started
-function leaveEncounter() {
-	document.getElementById("site").style.display = "block";
-	document.getElementById("map").style.display = "block";
-}
